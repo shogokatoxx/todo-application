a16ddc51c956fc727ff7cf8188de43de19ca355b
shogokatoxx <syogo_br@yahoo.co.jp>
2019-05-28 08:51:19 +0000

    settings更新(allow-host,timezone,language,static_root)

--- projects/settings.py
+++ projects/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_1z_xr3mrpg9wobi95jr!)&f_13a%t_f=w16ri-qckrgkxi9-%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASEDIR,'static')
