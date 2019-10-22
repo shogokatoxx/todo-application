ac32821125e492b11577f3191437dbe0362628f0
shogokatoxx <syogo_br@yahoo.co.jp>
2019-10-22 10:01:30 +0000

    cssをファイル一つにまとめる。少しリンクのスタイル調整

--- projects/settings.py
+++ projects/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_NAME = os.path.basename(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,4 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = '/var/www/{}/static'.format(PROJECT_NAME)
